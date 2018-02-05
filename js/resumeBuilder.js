6e0eccd9b59bfd7ddec10a9f3de3b4aa7be505bf
Taylor Weiss <tweiss@microsoft.com>
2018-02-04 17:39:07 -0800

    Prepend header data to keep contacts list at the end.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,5 +5,5 @@ Your code to build the resume will go here.
 // Format Data, Build a Resume Header
 var formattedName = HTMLheaderName.replace('%data%', 'Taylor Weiss');
 var formattedRole = HTMLheaderRole.replace('%data%', 'Software Engineer');
-$('#header').append(formattedName);
-$('#header').append(formattedRole);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
