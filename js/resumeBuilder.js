9d89976c4cc0ca38560380c78e3c095744fd87ef
Taylor Weiss <tweiss@microsoft.com>
2018-02-04 17:35:46 -0800

    Complete quiz to format the header.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,8 @@
 Your code to build the resume will go here.
  */
 
-$("#main").append("Taylor Weiss");
+// Format Data, Build a Resume Header
+var formattedName = HTMLheaderName.replace('%data%', 'Taylor Weiss');
+var formattedRole = HTMLheaderRole.replace('%data%', 'Software Engineer');
+$('#header').append(formattedName);
+$('#header').append(formattedRole);
